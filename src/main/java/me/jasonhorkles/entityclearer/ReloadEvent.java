c31479791d883c53efdb45866c0126d5206a441a
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-08-03 13:12:10 -0600

    Make server reload warning also send message in chat

--- src/main/java/me/jasonhorkles/entityclearer/ReloadEvent.java
+++ src/main/java/me/jasonhorkles/entityclearer/ReloadEvent.java
@@ -5,10 +5,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public record ReloadEvent(JavaPlugin plugin) implements Listener {
     @EventHandler
     public void onReload(ServerLoadEvent event) {
-        if (event.getType() == ServerLoadEvent.LoadType.RELOAD) plugin.getLogger().severe(
-            "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!");
+        if (event.getType() == ServerLoadEvent.LoadType.RELOAD) {
+            new Utils().sendError(
+                "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!");
+            plugin.getLogger().log(Level.SEVERE,
+                "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!");
+        }
     }
 }

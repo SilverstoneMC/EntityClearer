018e6ea095dce9a244e8be3af9d338b7cca56709
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-03-16 13:58:22 -0600

    Improve reload logic

--- src/main/java/me/jasonhorkles/entityclearer/ReloadEvent.java
+++ src/main/java/me/jasonhorkles/entityclearer/ReloadEvent.java
@@ -8,14 +8,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
-public record ReloadEvent(JavaPlugin plugin) implements Listener {
+public class ReloadEvent implements Listener {
+    public ReloadEvent(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    private final JavaPlugin plugin;
+    
     @EventHandler
     public void onReload(ServerLoadEvent event) {
         if (event.getType() == ServerLoadEvent.LoadType.RELOAD) {
-            new LogDebug().error(
-                "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!");
-            plugin.getLogger().log(Level.SEVERE,
-                "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!");
+            String message = "Server reload detected - things may break! Please restart your server as you should never use /reload on a production server!";
+
+            new LogDebug().error(message);
+            plugin.getLogger().log(Level.SEVERE, message);
         }
     }
 }

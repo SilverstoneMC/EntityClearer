9cb04680e4cc016e7d9a692cacc5fc01d4a28cda
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-04-09 14:08:40 -0600

    Copied from discord bot, will make it actually work later

--- src/main/java/me/jasonhorkles/entityclearer/utils/LogDebug.java
+++ src/main/java/me/jasonhorkles/entityclearer/utils/LogDebug.java
@@ -9,6 +9,14 @@ import org.bukkit.entity.Player;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.logging.Level;
 
 public class LogDebug {
@@ -39,5 +47,74 @@ public class LogDebug {
                 Component.text("[EntityClearer] " + worldName + ": " + message, NamedTextColor.RED));
 
         debug(Level.SEVERE, worldName, message);
+    }
+
+    public void upload() {
+        // Build the json
+        try {
+            JSONObject json = new JSONObject();
+            json.put("name", "Silverstone");
+            json.put("visibility", "unlisted");
+            json.put("expires", Instant.now().plus(7, ChronoUnit.DAYS));
+
+            JSONArray files = new JSONArray();
+            for (Message.Attachment attachment : attachments) {
+                JSONObject file = new JSONObject();
+
+                boolean isLog = attachment.getFileExtension().equalsIgnoreCase("log");
+                if (isLog) file.put("name", attachment.getFileName().replace(".log", ".accesslog"));
+                else file.put("name", attachment.getFileName());
+
+                JSONObject content = new JSONObject();
+                content.put("format", "text");
+                try (InputStream bytes = attachment.getProxy().download().join()) {
+                    content.put("value", new String(bytes.readAllBytes(), StandardCharsets.UTF_8));
+                }
+                file.put("content", content);
+
+                files.put(file);
+            }
+            json.put("files", files);
+
+            // Send the request
+            URL url = new URL("https://api.paste.gg/v1/pastes");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection) con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            byte[] out = json.toString().getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            http.setRequestProperty(
+                "Authorization",
+                "Key " + new me.jasonhorkles.silverstone.Secrets().getPasteKey());
+            http.connect();
+            try (OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            // Read the response
+            InputStream input = http.getInputStream();
+            JSONObject returnedText = new JSONObject(new String(input.readAllBytes(),
+                StandardCharsets.UTF_8));
+
+            if (returnedText.getString("status").equals("success")) {
+                String id = returnedText.getJSONObject("result").getString("id");
+                event.getHook().editOriginal("<https://paste.gg/p/JasonHorkles/" + id + ">").queue();
+
+            } else if (returnedText.getString("status").equals("error")) event.getHook().editOriginal(
+                    "## Error: " + returnedText.getString("error") + "\n" + returnedText.getString("message"))
+                .queue();
+
+        } catch (Exception e) {
+            System.out.print(new me.jasonhorkles.silverstone.Utils().getTime(me.jasonhorkles.silverstone.Utils.LogColor.RED));
+            e.printStackTrace();
+            event.getHook()
+                .editOriginal("An error occurred while uploading the file(s)! (" + e.getMessage() + ")")
+                .queue();
+        }
     }
 }
